91bb8168232e070b02627c81cf89aae60214ece9
Dong pin Oh <dhehdqls@gmail.com>
2018-06-10 09:10:33 +0900

    delete makeRandomAction

--- src/bms/player/beatoraja/pattern/makeRandomAction.java
+++
@@ -1,9 +0,0 @@
-package bms.player.beatoraja.pattern;
-
-import java.util.List;
-
-public interface makeRandomAction {
-
-	public void makeRandom(int[] result, List<Integer> noteLane, List<Integer> toRandomLane);
-	
-}
